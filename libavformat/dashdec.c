f822bd33edcd5186e63a4f8f9683d9d291c896ed
AmuroEita <1071307515@qq.com>
2024-06-29 21:30:21 +0800

    abr v15

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -550,10 +550,10 @@ static int open_url(AVFormatContext *s, AVIOContext **pb, const char *url,
             proto_name = avio_find_protocol_name(url + 6);
     }
 
-    av_log(s, AV_LOG_ERROR, "Test segment URL: %s", url);
-
     if (!proto_name)
         proto_name = avio_find_protocol_name(url);
+
+    av_log(s, AV_LOG_ERROR, "Test segment URL: %s, proto name: %s", url, proto_name);
 
     if (!proto_name)
         return AVERROR_INVALIDDATA;
