466c5a814f9c4b0f2d017f1ac288889ffdfbf0a6
AmuroEita <1071307515@qq.com>
2024-07-30 15:01:42 +0800

    scripts

--- scripts/player-720p.sh
+++ scripts/player-720p.sh
@@ -1,1 +1,1 @@
-sudo ./ffplay_g -loglevel 56 -dst_host "175.178.244.172" -init_file "init.mp4" -stat_remark "gz" -abr -abr-params format=dash ffabr:http://175.178.244.172/godzilla/720p/manifest.mpd
+sudo ../ffplay_g -loglevel 56 -dst_host "175.178.244.172" -init_file "init.mp4" -stat_remark "gz" -abr -abr-params format=dash ffabr:http://175.178.244.172/godzilla/720p/manifest.mpd
